f3f5af978fd98dcfeee899e19e44ab4301d06ee5
scrammbledeg <89812745+scrammbledeg@users.noreply.github.com>
2025-04-24 01:02:02 +0000

    comment added

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Learning C++ 
-// Exercise 00_03
+// Exercise 00_03 w/ mods
 // Using the exercise files on GitHub, by Eduardo Corpeño 
 
 #include <iostream>
@@ -12,6 +12,7 @@ int main(){
     std::cout << "Enter number 2: " << std::flush;
     std::cin >> num_2;
     
+
     result = num_1 + num_2;
     
     std::cout << "The result of the addition is " << result << std::endl;
